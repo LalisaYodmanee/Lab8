b50c5a8e9087fc2cf784d721bc59241d80731463
Lalisa-Yodmanee <MJU6404101337@mju.ac.th>
2023-02-12 14:15:56 +0700

    Lab8_337

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+  <h1>HIGHLIGHTS</h1>
+    <h4>⊂◉‿◉つつRick and Morty recounts the adventures of drunken scientist Rick Sanchez and his nerdy grandson, Morty Smith, who is easily persuaded. The two travel back and forth between their home and other extraterrestrial dimensions. Along with the Smith family and various alien species.</h4>
+    <h4>✿◕ ‿ ◕✿ The inspiration of two main characters, Dr. Emmet Brown and Marty McFly, from the movie Back to the Future.</h4>
+
+
+    <h4>(◕‿◕) The show received a 97% review score on Rotten Tomatoes and a 96% audience score.
+  
+    </h4>
+    
+    
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/MovieView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div> 
+    <h1>
+    movie
+
+    </h1>
+    <button @click="Network">name</button>
+    <ul> <li v-for="movies in cartoon" :key="movies">
+        {{ movies.id }}{{ movies.name }} {{ movies.status }}
+        <h2> {{ movies.name}}</h2>
+         <h4> {{ movies.species}}</h4>
+         <h4> {{ movies.status}}</h4></li></ul>
+    </div>
+</template>
+<script setup>
+import axios from"axios";
+import { ref } from"vue";
+
+
+const cartoon = ref();
+const url = ref("https://rickandmortyapi.com/api/character/108");
+function Network() {
+    axios    
+    .get(url.value)    
+    .then((response) => {
+        cartoon.value = response.data;   
+    })    
+        .catch((err) => {console.log(err);    
+        });
+    }
+
+</script>
